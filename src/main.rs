b9c8cde5fc6819c6a7fb62481ab86065c2de0ac3
found-cake <skymin468@gmail.com>
2023-11-15 14:06:39 +0900

    3의 배수 이면서 5의 배수는 15의 배수

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     // 1 ~ 100
     for i in 1..101 {
         // 3의 배수 이면서 5의 배수
-        if i % 3 == 0 && i % 5 == 0 {
+        if i % 15 == 0 {
             println!("PizzBuzz");
         // 3의 배수
         } else if i % 3 == 0 {
